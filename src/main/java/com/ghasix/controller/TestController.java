ed3bb8b17fe1b4ff01a171e871e71eae192e05ec
RobiLee <robi9202@gmail.com>
2019-05-04 00:49:13 +0900

    DB스키마 추가 및 jsp뷰 테스트.

--- src/main/java/com/ghasix/controller/TestController.java
+++ src/main/java/com/ghasix/controller/TestController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class TestController {
@@ -11,7 +12,7 @@ public class TestController {
     private static Logger logger = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping("/test")
-    public String test() {
-        return "Hello Spring Boot!";
+    public ModelAndView test() {
+        return new ModelAndView("index");
     }
 }
